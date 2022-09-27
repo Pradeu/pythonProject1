d8e97bc87268ca1bd45c22c4b2478ce0f6015812
Pradeu <medovoi.dmitry@yandex.ru>
2022-09-27 12:07:50 +0300

    New

---
+++ Tasks/Monty_Hall.py
@@ -0,0 +1,16 @@
+import random
+a = [0, 1, 0]
+summa = 0
+total = 1000000
+#random(a)
+for i in range(total):
+    d = a.copy()
+    b = random.randint(0, 2)
+    if b == 0:
+        d.pop(2)
+    else:
+        d.pop(0)
+    if d[0] == 1:
+        summa = summa + 1
+percent = summa/total * 100
+print(f"Процент выигрыша равен {percent}")
