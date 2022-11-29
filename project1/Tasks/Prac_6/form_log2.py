d9e9c465e7ac7950e4024721403c1133ef63c38f
Pradeu <medovoi.dmitry@yandex.ru>
2022-11-29 12:14:13 +0300

    distortion

--- project1/Tasks/Prac_6/form_log2.py
+++ project1/Tasks/Prac_6/form_log2.py
@@ -6,6 +6,6 @@ form_log = open(f"Form_Log.txt", mode="w")
 pattern = r"(?:^Рейс) (\d+) (?:прибыл|отправился) (\w+) (\w+) (?:\w+) (\d+:\d+:\d+)"
 for info in train_log:
     form_info = re.findall(pattern, info)
-    print(f"{form_info}\n")
-    form_log.write(f"{form_info}")
+    print(f"{form_info}")
+    form_log.write(f"{form_info}\n")
 info = train_log.readline().rstrip('\n')
